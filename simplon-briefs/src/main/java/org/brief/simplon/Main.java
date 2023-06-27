463b6bb003985aa012b842e37908c5f7f141bf78
Abderaim Elouardy <elouardy.abderrahim07@gmail.com>
2023-06-27 01:04:02 +0100

    static key word

---
+++ simplon-briefs/src/main/java/org/brief/simplon/Main.java
@@ -0,0 +1,22 @@
+package org.brief.simplon;
+
+public class Main {
+    public static void main(String[] args) {
+        // static = modifier. A single copy of a variable/method is created and shared.
+
+        //			The class "owns" the static member
+
+
+
+        StaticKeyWord friend1 = new StaticKeyWord("Sponegbob");
+
+        StaticKeyWord friend2 = new StaticKeyWord("Patrick");
+
+        StaticKeyWord friend3 = new StaticKeyWord("Patrick");
+
+        StaticKeyWord.desplayNumOfFriend();
+
+
+        //System.out.println(StaticKeyWord.numberOfFriends);
+    }
+}
